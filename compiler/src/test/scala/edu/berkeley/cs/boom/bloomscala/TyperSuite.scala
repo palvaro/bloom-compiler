298c86fdac9d36c6d418f5849eba7c0cc7be54b3
Thomas Santana <mailleux@gmail.com>
2015-05-19 22:21:38 -0300

    Check compiler error messages in tests.

--- compiler/src/test/scala/edu/berkeley/cs/boom/bloomscala/TyperSuite.scala
+++ compiler/src/test/scala/edu/berkeley/cs/boom/bloomscala/TyperSuite.scala
@@ -1,32 +1,34 @@
 package edu.berkeley.cs.boom.bloomscala
 
-
-
 class TyperSuite extends BloomScalaSuite {
 
   test("NotIn schemas should match") {
-    intercept[CompilerException] { Compiler.compileToIntermediateForm(
-      """
-        | table a, [val: int]
-        | table b, [val: string]
-        | a <= a.notin(b)
-      """.stripMargin)
+    intercept[CompilerException] {
+      compileSource(
+        """
+          | table a, [val: int]
+          | table b, [val: string]
+          | a <= a.notin(b)
+        """.stripMargin)
     }
+    emitter.assertContainFuzzy("[4.7] notin called with incompatible schemas:\n[int]\n[string]")
   }
 
   test("NotIn LHS and RHS schemas should match") {
-    intercept[CompilerException] { Compiler.compileToIntermediateForm(
-      """
-        | table a, [val: int]
-        | table b, [val: int]
-        | table c, [val: string]
-        | c <= a.notin(b)
-      """.stripMargin)
+    intercept[CompilerException] {
+      compileSource(
+        """
+          | table a, [val: int]
+          | table b, [val: int]
+          | table c, [val: string]
+          | c <= a.notin(b)
+        """.stripMargin)
     }
+    emitter.assertContainFuzzy("has wrong schema; expected [string] but got [int]")
   }
 
   test("Typing 3-way join") {
-    Compiler.compileToIntermediateForm(
+    compileSource(
       """
         | table a, [val: int]
         | table b, [val: int]
@@ -36,42 +38,50 @@ class TyperSuite extends BloomScalaSuite {
   }
 
   test("argmin ordering type must unify with ordering field type") {
-    intercept[CompilerException] { Compiler.compileToIntermediateForm(
-      """
-        | table a, [key: int, val: int]
-        | table b, [key: int, val: int]
-        | b <= a.argmin([a.key], a.val, stringOrder)
-      """.stripMargin)
+    intercept[CompilerException] {
+      compileSource(
+        """
+          | table a, [key: int, val: int]
+          | table b, [key: int, val: int]
+          | b <= a.argmin([a.key], a.val, stringOrder)
+        """.stripMargin)
     }
+    emitter.assertContainFuzzy("[4.7] expected partial order over int, but found function 'stringOrder' of type (string, string) -> boolean")
   }
 
   test("cannot insert into inputs") {
-    intercept[CompilerException] { Compiler.compileToIntermediateForm(
-      """
-        | input a, [key: int, val: int]
-        | table b, [key: int, val: int]
-        | a <= b
-      """.stripMargin)
+    intercept[CompilerException] {
+      compileSource(
+        """
+          | input a, [key: int, val: int]
+          | table b, [key: int, val: int]
+          | a <= b
+        """.stripMargin)
     }
+    emitter.assertContainFuzzy("[4.2] Cannot insert into collections of type 'Input'")
   }
 
   test("cannot read from outputs") {
-    intercept[CompilerException] { Compiler.compileToIntermediateForm(
-      """
-        | output a, [key: int, val: int]
-        | table b, [key: int, val: int]
-        | b <= a
-      """.stripMargin)
+    intercept[CompilerException] {
+      compileSource(
+        """
+          | output a, [key: int, val: int]
+          | table b, [key: int, val: int]
+          | b <= a
+        """.stripMargin)
     }
+    emitter.assertContainFuzzy("[4.2] Output collections cannot appear in the RHS of rules")
   }
 
   test("outputs only support asynchronous merge") {
-    intercept[CompilerException] { Compiler.compileToIntermediateForm(
-      """
-        | output a, [key: int, val: int]
-        | table b, [key: int, val: int]
-        | a <= b
-      """.stripMargin)
+    intercept[CompilerException] {
+      compileSource(
+        """
+          | output a, [key: int, val: int]
+          | table b, [key: int, val: int]
+          | a <= b
+        """.stripMargin)
     }
+    emitter.assertContainFuzzy("[4.2] Output collections only support the <~ operator")
   }
 }
